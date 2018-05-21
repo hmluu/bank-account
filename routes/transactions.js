871866a5fb7c89532033c7b06bdc80d45d6f3b9d
Hoa Luu <hoamluu@gmail.com>
2018-05-21 10:04:24 -0400

    added /

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,6 +6,6 @@ router.get('/accounts/:account_id/transactions', transactionsController.index);
 router.get('/transactions/:transaction_id', transactionsController.show);
 router.post('/accounts/:account_id/transactions', transactionsController.create);
 router.put('/transactions/:transaction_id', transactionsController.update);
-router.delete('transactions/:transaction_id', transactionsController.destroy); //no need to add books after / because of index.js
+router.delete('/transactions/:transaction_id', transactionsController.destroy); //no need to add books after / because of index.js
 
 module.exports = router;
